e8111e8e02540d25d8547f9406fdeb19f722f8ab
training_C2D.02.11 <training_C2D.02.11@accenture.com>
2019-07-25 15:33:42 +0530

    added files

---
+++ src/test/java/cucumberDevops/S1test.java
@@ -0,0 +1,13 @@
+package cucumberDevops;
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/s1.feature",
+				//glue="packagename"
+				plugin="json:target\\jsonreport.jason")
+public class S1test {
+
+}
---
+++ src/test/java/cucumberDevops/StepTest.java
@@ -0,0 +1,44 @@
+package cucumberDevops;
+	import org.junit.Assert;
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+
+	import cucumber.api.java.en.Given;
+	import cucumber.api.java.en.Then;
+	import cucumber.api.java.en.When;
+	public class StepTest {
+		WebDriver driver;
+		@Given("user opens login portal")
+		public void user_opens_login_portal() {
+			System.setProperty("webdriver.chrome.driver", "C:\\\\Saravanan training\\\\chromedriver.exe");
+			  driver=new ChromeDriver();
+			  driver.get("http://newtours.demoaut.com");
+		}
+
+		@When("user enters username as {string}")
+		public void user_enters_username_as(String un) {
+		 // System.out.println("enter username as "+un);
+		  driver.findElement(By.name("userName")).sendKeys(un);
+		}
+
+		@When("user enters password as {string}")
+		public void user_enters_password_as(String psd) {
+		   //System.out.println("enter password as "+psd);
+		  driver.findElement(By.name("password")).sendKeys(psd);
+		}
+
+		@When("user Clicks on singin button")
+		public void clicks_on_singn_button() {
+		  // System.out.println("click signin");
+			 driver.findElement(By.xpath("//input[@value='Login']")).click();
+		}
+
+		@Then("validate login success")
+		public void validate_login_success() {
+		    //System.out.println("login sucess");
+			Assert.assertTrue(driver.getTitle().contains("Flight"));
+			  driver.close();
+		}
+
+	}
